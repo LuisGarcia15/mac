b6efcf1c28833e7deead007f7749f89282efce21
Luis <luis200107@icloud.com>
2022-10-08 12:31:58 -0500

    Probando desde lenovo

--- src/aragon/unam/tareas/tareaCinco/PruebasTareaSeis.java
+++ src/aragon/unam/tareas/tareaCinco/PruebasTareaSeis.java
@@ -4,7 +4,7 @@ import java.util.Iterator;
 
 import aragon.unam.estructuras.ArrayADT;
 
-public class PruebasTarea5 {
+public class PruebasTareaSeis {
 	public static void main(String[] args) {
 		Laberinto l = new Laberinto();
 	}
